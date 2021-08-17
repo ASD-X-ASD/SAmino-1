cf7b3698daaf422b8ec6e08c8dc286a6ce73cdbb
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-17 19:03:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-from SAmino import samino
+import BotAmino
 
-client = samino.Client("2221B00E164AEFA3EDF43EAE05BF12D5BBA90DFF5A955F77C5775DDE815C33F98510E23DC0F8116097")
-client.login("heromr59@azel.xyz","Zezo1992")
+client = BotAmino("heromr59@azel.xyz","Zezo1992")
+
 client.close()
 client.start()
 print("Done")
