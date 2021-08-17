752461e0a9af2ec0dbdb8a2c525fd682b3589e71
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-17 18:49:04 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from SAmino improt samino
+from SAmino import samino
 
 client = samino.Client("222DE58327EEF26EC0BC71BD9AA7955BD823A8E7311D12F3D72DF70C5C0FCA38EF5D0F81D463199FC5")
 client.login("heromr59@azel.xyz","Zezo1992")
