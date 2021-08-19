d91d7d31b2b679425b48321b83f7613d703ddd61
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-19 19:29:49 +0300

    Update test.py

--- test.py
+++ test.py
@@ -36,7 +36,7 @@ def socketRoot():
 		client.launch()
 		shundle = client.socket
 		shundle.close()
-		client.launch()
+		#client.close()
 		print("updated socket!")
 		time.sleep(300)
 socketRoot()
