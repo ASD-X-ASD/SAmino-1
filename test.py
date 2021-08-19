3c2d5e3926ca386596c40bd817f0d702687f5f4e
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-19 18:43:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,41 +7,39 @@ client.login(email="heromr59@azel.xyz",password="Zezo1992",isWeb=True)
 
 @client.event("on_message")
 def on_message(data: samino.lib.Event):
-    msg = data.message.content
-    msgId = data.message.messageId
-    comId = data.ndcId
-    chatId = data.message.chatId
-    userId = data.message.userId
-    nickname = data.message.author.nickname
-    try: mentionIds = data.message.json["extensions"]["mentionedArray"]
-    except: pass
-    local = samino.Local(comId)
-
-    if msg.startswith("!tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2" :
-        local.send_message(chatId=chatId, f"لقد حصلت على جائزتك اليومية ",isWeb=True)
-        for a in range(250): client.watch_ad(userId)
-
-
-    if userId in vip:
-        if msg.startswith("#follow"):
-            for user in mentionIds:
-                local.follow(user["uid"], isWeb=True)
-                local.send_message(chatId, "تم متابعة العضو", isWeb=True)
-
-        if msg.startswith("#unfollow"):
-            for user in mentionIds:
-                local.unfollow(user["uid"], isWeb=True)
-                local.send_message(chatId, "تم إلغاء متابعة العضو", isWeb=True)
+	msg = data.message.content
+	msgId = data.message.messageId
+	comId = data.ndcId
+	chatId = data.message.chatId
+	userId = data.message.userId
+	nickname = data.message.author.nickname
+	try: mentionIds = data.message.json["extensions"]["mentionedArray"]
+	except: pass
+	local = samino.Local(comId)
+	
+	if msg.startswith("!tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2" :
+		local.send_message(chatId=chatId, f"لقد حصلت على جائزتك اليومية ",isWeb=True)
+		for a in range(250):
+			client.watch_ad(userId)
+			
+	if userId in vip:
+		if msg.startswith("#follow"):
+			for user in mentionIds:
+				local.follow(user["uid"], isWeb=True)
+				local.send_message(chatId, "تم متابعة العضو", isWeb=True)
+	if msg.startswith("#unfollow"):
+		for user in mentionIds:
+			local.unfollow(user["uid"], isWeb=True)
+			local.send_message(chatId, "تم إلغاء متابعة العضو", isWeb=True)
 
 
 def socketRoot():
-    j = 0
-    while True:
-        print("updating socket....")
-        client.launch()
-        shundle = client.socket
-        shundle.close()
-        client.launch()
-        print("updated socket!")
-        time.sleep(300)
+	while True:
+		print("updating socket....")
+		client.launch()
+		shundle = client.socket
+		shundle.close()
+		client.launch()
+		print("updated socket!")
+		time.sleep(300)
 socketRoot()
