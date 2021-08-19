eb1bb54e444a856652feb0ce4760d98cbda1fc79
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-19 18:39:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -17,9 +17,10 @@ def on_message(data: samino.lib.Event):
     except: pass
     local = samino.Local(comId)
 
-    if msg.startswith("!tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2" : # every command 2 coin
-        for a in range(200): client.watch_ad(userId)
+    if msg.startswith("!tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2" :
         local.send_message(chatId=chatId, f"لقد حصلت على جائزتك اليومية ",isWeb=True)
+        for a in range(250): client.watch_ad(userId)
+
 
     if userId in vip:
         if msg.startswith("#follow"):
