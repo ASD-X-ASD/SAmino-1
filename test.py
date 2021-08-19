1234d8fac0b0a428c9147bebfae877f0f9211514
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-19 18:12:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,36 @@
-import BotAmino
+import samino
 
-client = BotAmino("heromr59@azel.xyz","Zezo1992")
+vip = ["f63fc94e-324c-4181-b87f-c2a500a0b23b"] # userIds
+client = samino.Client("22210FBEEEA9D9C77872C1D9E57892F6CE987064D3B9EA712461480F639FFD4AFC4B33191E466EDB9D")
+client.login(email="heromr59@azel.xyz",password="Zezo1992",isWeb=True)
 
-client.close()
-client.start()
-print("Done")
+
+@client.event("on_message")
+def on_message(data: samino.lib.Event):
+    msg = data.message.content
+    msgId = data.message.messageId
+    comId = data.ndcId
+    chatId = data.message.chatId
+    userId = data.message.userId
+    nickname = data.message.author.nickname
+    try: mentionIds = data.message.json["extensions"]["mentionedArray"]
+    except: pass
+    local = samino.Local(comId)
+
+    if msg.startswith("#tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2" : # every command 2 coin
+        client.watch_ad(userId)
+        local.send_message(chatId, f"<${nickname}@$> لقد حصلت على جائزتك اليومية ",replyTo=msgId,isWeb=True)
+
+    if userId in vip:
+        if msg.startswith("#follow"):
+            for user in mentionIds:
+                local.follow(user["uid"], isWeb=True)
+                local.send_message(chatId, "تم متابعة العضو", isWeb=True)
+
+        if msg.startswith("#unfollow"):
+            for user in mentionIds:
+                local.unfollow(user["uid"], isWeb=True)
+                local.send_message(chatId, "تم إلغاء متابعة العضو", isWeb=True)
+
+
+client.launch()
