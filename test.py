da26733804f0309c01019863a33a72d56c95d0ed
HeroMr0 <87385132+HeroMr0@users.noreply.github.com>
2021-08-20 09:51:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ vip = ["f63fc94e-324c-4181-b87f-c2a500a0b23b"] # userIds
 client = samino.Client("22210FBEEEA9D9C77872C1D9E57892F6CE987064D3B9EA712461480F639FFD4AFC4B33191E466EDB9D")
 client.login(email="heromr59@azel.xyz",password="Zezo1992",isWeb=True)
 
+wikipedia.set_lang('ar')
+
 @client.event("on_message")
 def on_message(data: samino.lib.Event):
 	msg = data.message.content
@@ -18,7 +20,10 @@ def on_message(data: samino.lib.Event):
 	if msg.startswith("!tap") and chatId == "a1d77860-084e-40cf-855d-228d0fb333f2":
 		local.send_message(chatId,f"{nickname} لقد حصلت على جائزتك اليومية",asWeb=True)
 		for a in range(300): client.watch_ad(userId)
-			
+	if content.startswith("!search"): st = wikipedia.search(content[8:]);s = wikipedia.summary(st[0]);local.send_message(chatId=chatId, message=f"""
+[BC]{st[0]}  
+  
+[C]{s[0:1990]}""",asWeb=True)
 	if userId in vip:
 		if msg.startswith("!follow"):
 			for user in mentionIds:
